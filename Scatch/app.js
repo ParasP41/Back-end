be3b19df4575c8484836e2cdffe1e7a57f13cd38
ParasP41 <parasvishwakarma198@gmail.com>
2025-04-29 11:18:46 +0530

    server

--- Scatch/app.js
+++ Scatch/app.js
@@ -1,6 +1,10 @@
 const express = require('express')
 const path=require('path');
 const cookieParser=require('cookie-parser')
+const db=require('./config/mongooseConnection.js')
+const ownersRouter=require('./routes/ownersRouter.js')
+const usersRouter=require('./routes/usersRouter.js')
+const productsRouter=require('./routes/productsRouter.js')
 const app = express()
 const port = 3000
 
@@ -9,6 +13,10 @@ app.use(express.urlencoded({extended:true}))
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname,'./public')))
 app.set('view engine','ejs');
+
+app.use('/owners',ownersRouter);
+app.use('/users',usersRouter);
+app.use('/products',productsRouter);
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
---
+++ Scatch/routes/ownersRouter.js
@@ -0,0 +1,29 @@
+const express = require('express');
+const router = express.Router();
+const ownerModel = require('../models/ownerModel.js')
+require('dotenv').config();
+router.get('/', (req, res) => {
+  res.send('Hello World!')
+})
+
+
+if (process.env.NODE_ENV === "developement") {
+  router.post('/create', async (req, res) => {
+    let owner = await ownerModel.find()
+    if (owner.length > 0) {
+     return res.status(502).send("you dont have permission to create a new owner")
+    }
+
+    let {fullname,email,password}=req.body;
+
+    let createdOwner=await ownerModel.create({
+      fullname,
+    email,
+    password,
+    })
+    res.send(createdOwner);
+  })
+}
+
+module.exports = router;
+
